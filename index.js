ff6a4ada6860dfe30f5f55439d4fe7351a577dbe
Muharem Čorbadžić <muharem.corbadzic@gmail.com>
2022-04-28 19:11:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 4545;
+
+app.get('/api/healt', (req, res) => 
+  res.json({ 'status': 'ok' }));
+
+//app.listen(port, () => console.log(`Node app listening on port ${port}!`))
