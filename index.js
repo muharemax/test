4c7f14a1e5e3b614bfa2edc8351db7beda44912b
Muharem Čorbadžić <muharem.corbadzic@gmail.com>
2022-04-28 22:17:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 4545;
 
-app.get('/api/healt', (req, res) => 
+app.get('/api/health', (req, res) => 
   res.json({ 'status': 'ok' }));
 
 //app.listen(port, () => console.log(`Node app listening on port ${port}!`))
